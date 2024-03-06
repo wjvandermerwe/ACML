fix the output delta calculation

ACML_LAB3/nn_method.py
@@ -1,5 +1,7 @@
 import torch
-
+from ucimlrepo import fetch_ucirepo
+import ssl
+import numpy as np
 class MultiLayerPerceptron():
     def __init__(self, input_size, hidden_size, output_size):
         self.lr = 0.01
@@ -10,16 +12,20 @@ class MultiLayerPerceptron():
         self.biases2 = torch.randn(output_size)
 
     def forward(self, X):
+        X = X.float()
+
         # activations
         self.hidden = torch.sigmoid(torch.matmul(X, self.weights1) + self.biases1)
         self.output = torch.sigmoid(torch.matmul(self.hidden, self.weights2) + self.biases2)
+
         return self.output
 
     def tail(self, s):
         return s * (1 - s)
     def backward(self, X, Y, output):
+        X = X.float()
         # deltas
-        self.delta_output = Y - output * self.tail(output)
+        self.delta_output = ((Y - output) * self.tail(output)).float()
         self.delta_hidden = torch.matmul(self.delta_output, self.weights2.T) * self.tail(self.hidden)
 
         # weights and biases
@@ -42,19 +48,44 @@ class MultiLayerPerceptron():
 
 input_size = 4
 hidden_size = 4
-output_size = 2
+output_size = 3
 
 mlp = MultiLayerPerceptron(input_size, hidden_size, output_size)
-inputs = torch.randn(20, 4)
-labels = torch.randn(20, 2)
+# inputs = torch.randn(20, 4)
+# labels = torch.randn(20, 3)
+ssl._create_default_https_context = ssl._create_unverified_context
+iris = fetch_ucirepo(id=53)
+X = iris.data.features
+y = iris.data.targets.values.flatten()
+unique_vals = np.unique(y).tolist()
+n_classes = len(unique_vals)
+one_hot_matrix = np.zeros((len(y), n_classes))
 
-max_iterations = 5000
+inputs = torch.tensor(X.values)
+for i, val in enumerate(y):
+    index = unique_vals.index(val)
+    one_hot_matrix[i, index] = 1
+
+labels = torch.tensor(one_hot_matrix)
+
+max_iterations = 2000
 log_interval = 20
 
 loss = 1
 iteration = 0
 while loss > 0 and max_iterations > iteration:
-    loss = mlp.train(inputs, labels)
+    indices = torch.arange(len(inputs))
+    shuffled_indices = torch.randperm(len(indices))
+
+    # Use the shuffled indices to reorder both tensors
+    shuffled_tensor1 = inputs[shuffled_indices]
+    shuffled_tensor2 = labels[shuffled_indices]
+    loss = mlp.train(shuffled_tensor1, shuffled_tensor2)
     if iteration % log_interval == 0:
         mlp.log_output(iteration, loss)
     iteration += 1
+
+
+predict_input = torch.tensor([5.1000, 3.5000, 1.4000, 0.2000]).unsqueeze(0)
+
+print(mlp.forward(predict_input))