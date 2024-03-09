7babfc364b2e928277c7f7bbfe734acecdf75430
johandieman <johanvdmerwelus@gmail.com>
2024-03-09 13:43:04 +0200

    upload information

--- ACML_LAB2/iris_data.py
+++ ACML_LAB2/iris_data.py
@@ -1,6 +1,7 @@
 from ucimlrepo import fetch_ucirepo, list_available_datasets
 import ssl
 import numpy as np
+import torch
 
 # Ignore ssl certificate verification
 ssl._create_default_https_context = ssl._create_unverified_context
@@ -9,6 +10,8 @@ X = iris.data.features
 y = iris.data.targets.values.flatten()
 # print(y)
 
+inputs = torch.tensor(X.values)
+print(inputs)
 
 unique_vals = np.unique(y).tolist()
 n_classes = len(unique_vals)
@@ -17,6 +20,8 @@ one_hot_matrix = np.zeros((len(y), n_classes))
 for i, val in enumerate(y):
     index = unique_vals.index(val)
     one_hot_matrix[i, index] = 1
+labels= torch.tensor(one_hot_matrix)
+# print(labels)
 
-print(one_hot_matrix)
+# print(one_hot_matrix)
 
