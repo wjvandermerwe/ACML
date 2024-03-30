735b875f545a86c83bda30ffa8a7ff942eaf42ba
johandieman <johanvdmerwelus@gmail.com>
2024-03-30 09:42:50 +0200

    add test info

--- week5/Assignment1/nn_module.py
+++ week5/Assignment1/nn_module.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import argparse
 class MultiLayerPerceptron:
     def __init__(self, input_size, hidden_size, output_size):
         self.lr = 0.1
@@ -45,10 +45,25 @@ input_size = 4
 hidden_size = 8
 output_size = 3
 
-inputs = np.array([-2, 1, 0.5, -1])
-labels = np.array([0, 0, 1])
+# inputs = np.array([-2, 1, 0.5, -1])
+# labels = np.array([0, 0, 1])
 # inputs = np.array([1.4, 0, -2.5, -3])
 # labels = np.array([0.4, 0.6, 0])
+
+def parse_numbers(arg_str):
+    numbers = [float(x) for x in arg_str.split(',')]
+    if len(numbers) != 7:
+        raise argparse.ArgumentTypeError("Exactly seven numbers are required.")
+    return np.array(numbers[:4]), np.array(numbers[4:])
+
+parser = argparse.ArgumentParser(description='Run a forward and backward pass of MLP.')
+parser.add_argument('numbers', type=parse_numbers,
+                    help='Comma-separated list of seven numbers, where the first four are inputs and the last three are labels (e.g., "1.4,0,-2.5,-3,0.4,0.6,0")')
+
+args = parser.parse_args()
+
+# Separate the inputs and labels based on the single input argument
+inputs, labels = args.numbers
 mlp = MultiLayerPerceptron(input_size, hidden_size, output_size)
 
 
